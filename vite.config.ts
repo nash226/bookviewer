fb048ea7700bc11278766604058be966a00bcdca
Nazeer Shaikh <nazeer.shaikh54@gmail.com>
2026-02-04 18:37:42 -0500

    added 'www.luudo.org' as a host

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['luudo.org'],
+    allowedHosts: ['luudo.org', 'www.luudo.org'],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
